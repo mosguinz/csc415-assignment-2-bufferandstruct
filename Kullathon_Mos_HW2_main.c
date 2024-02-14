538fdbb2dd1692e6124050d6fb531169a18fd2b3
mosguinz <mos.guinz@gmail.com>
2024-02-13 16:29:48 -0800

    Complete steps 1 through 5

---
+++ Kullathon_Mos_HW2_main.c
@@ -0,0 +1,65 @@
+/**************************************************************
+ * Class::  CSC-415-03 Spring 2024
+ * Name::  Mos Kullathon
+ * Student ID::  921425216
+ * GitHub-Name::  mosguinz
+ * Project:: Assignment 2 – Command Line Arguments
+ *
+ * File::  Kullathon_Mos_HW2_main.c
+ *
+ * Description::
+ *
+ **************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <assignment2.h>
+
+int main(int argc, char *argv[])
+{
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%d. %s\n", i, argv[i]);
+    }
+    if (argc < 4)
+    {
+        return -1;
+    }
+
+    // Step four is to allocate (using malloc) an instantiation of the
+    // personInfo structure and to populate it.
+    personalInfo *person = (personalInfo *)malloc(sizeof(personalInfo));
+
+    // The firstName and the lastName are populated from the 1st
+    // and second command line argument.
+    person->firstName = argv[1];
+    person->lastName = argv[2];
+
+    // You will then assign your student ID to the studentID field,
+    // you will populate the level (gradelevel) appropriately.
+    person->studentID = 921425216;
+    person->level = JUNIOR;
+
+    // You will then populate the languages field.
+    person->languages = (KNOWLEDGE_OF_JAVA | KNOWLEDGE_OF_CPLUSPLUS | KNOWLEDGE_OF_MIPS_ASSEMBLER);
+
+    // The last part of populating the structure is to copy the third
+    // command line parameter to the message field.
+    strncpy(person->message, argv[3], 100);
+
+    // Step five is to "write" your personal information structure
+    // by calling writePersonalInfo
+    if (writePersonalInfo(person) != 0)
+    {
+        // print error here
+        return -2;
+    }
+
+    free(person);
+    checkIt();
+
+    return 0;
+}
